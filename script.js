7a41bbd84fd284346e9f2f81cf51d538499acdab
Adam Grzesik <agrzesik88@gmail.com>
2023-11-22 20:33:27 +0100

    animacje logo - gotowe

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Nasłuchujemy zdarzenie przewijania strony
 window.addEventListener('scroll', function() {
-    const logo = document.querySelector('.logo_big');
+        const logoBig = document.querySelector('.logo_big');
+        const logoMini = document.querySelector('.logo_mini');
     if (window.scrollY > 5) { // Wartość, po której ma nastąpić animacja (przykładowo 50px)
-        logo.style.top = '-150px'; // Gdy zaczynamy scrollować w dół, logo przesuwa się do góry
+        logoBig.style.top = '-200px'; // Gdy zaczynamy scrollować w dół, logo przesuwa się do góry
+        logoMini.style.top = '32px'; // Pokazanie logo_mini
+        logoMini.style.left = '40%'; // Pokazanie logo_mini
     } else {
-        logo.style.top = '108px'; // Gdy wracamy do góry, logo wraca na pierwotne położenie
+        logoBig.style.top = '108px'; // Gdy wracamy do góry, logo wraca na pierwotne położenie
+        logoMini.style.top = '-180px'; // Schowanie logo_mini
     }
 });
